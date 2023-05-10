728028c8e191a49949e724e7ffcc214719e81fbd
Roman <romanProger@mail.ru>
2023-05-10 02:32:39 +0000

    Убрал ненужный прокси

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: 'http://188.143.130.20:3128'
-  }
+  transpileDependencies: true
 })
