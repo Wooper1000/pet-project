4c511eb1f6de196eed84b0455f9678a9543f7b69
Roman <romanProger@mail.ru>
2023-05-16 22:26:22 +0000

    Переделал иконки в настройках профиля

--- src/components/User-Profile-Settings-App.vue
+++ src/components/User-Profile-Settings-App.vue
@@ -1,25 +1,26 @@
 <template>
     <v-app class="px-2">
-        <v-container class="container settings-page">
-            <v-bottom-navigation grow :elevation="0">
-                <v-btn value="recent">
-                    <v-icon>mdi-cog-outline</v-icon>
-                    Настройки
-                </v-btn>
-
-                <v-btn value="favorites">
-                    <v-icon>mdi-view-agenda-outline</v-icon>
-                    Задачи
-                </v-btn>
-
-                <v-btn value="nearby" @click="$router.push('/profile/information')">
-                    <v-icon>mdi-account-outline</v-icon>
-                    Профиль
-                </v-btn>
-            </v-bottom-navigation>
-            <v-divider class="over-driver"></v-divider>
+        <v-container class="container">
+            <div class="top-nav text-center mb-1">
+                <v-row>
+                    <v-col cols="4">
+                        <v-icon icon="pet:settings"></v-icon>
+                        <div class="text-body-2">Настройки</div>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-icon icon="pet:server"></v-icon>
+                        <div class="text-body-2">Задачи</div>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-icon icon="pet:user" color="primary"></v-icon>
+                        <div class="text-body-2 text-primary">Профиль</div>
+                    </v-col>
+                </v-row>
+            </div>
+            <v-divider class="over-driver mb-4"></v-divider>
             <v-list>
-                <v-list-item class="avatar-item" title="Иннокентий Пупырчатый" subtitle="ivanov@mail.ru" variant="tonal" @click="$router.push('/profile/information')">
+                <v-list-item class="avatar-item" title="Иннокентий Пупырчатый" subtitle="ivanov@mail.ru" variant="tonal"
+                    @click="$router.push('/profile/information')">
                     <template v-slot:prepend>
                         <v-avatar
                             image="https://sun9-36.userapi.com/impg/9EGAXV98mSehy5617zZTg5Xw176v7I0Yd4xpXw/Hhtf-62aVL8.jpg?size=2560x1701&quality=95&sign=e9084093657bd7ec3c472da12da7ca23&type=album"
@@ -27,43 +28,43 @@
                         </v-avatar>
                     </template>
                     <template v-slot:append>
-                        <v-btn variant="text" icon="mdi-chevron-right" color="grey-lighten-1"></v-btn>
+                        <v-btn variant="text" icon="pet:chevron-right" color="grey-lighten-1"></v-btn>
                     </template>
                 </v-list-item>
                 <v-list-item :title="$t('language')" @click="$router.push('/settings/language')">
                     <template v-slot:prepend>
-                        <v-icon color="primary" class="settings-icon">mdi-web</v-icon>
+                        <v-icon icon="pet:globe" class="settings-icon" color="primary"></v-icon>
                     </template>
 
                     <template v-slot:append>
-                        <v-btn color="grey-lighten-1" icon="mdi-chevron-right" variant="text"></v-btn>
+                        <v-btn color="grey-lighten-1" icon="pet:chevron-right" variant="text"></v-btn>
                     </template>
                 </v-list-item>
                 <v-list-item :title="$t('geo')" @click="$router.push('/settings/geo')">
                     <template v-slot:prepend>
-                        <v-icon color="primary">mdi-compass-outline</v-icon>
+                        <v-icon icon="pet:compass" class="settings-icon" color="primary"></v-icon>
                     </template>
 
                     <template v-slot:append>
-                        <v-btn color="grey-lighten-1" icon="mdi-chevron-right" variant="text"></v-btn>
+                        <v-btn color="grey-lighten-1" icon="pet:chevron-right" variant="text"></v-btn>
                     </template>
                 </v-list-item>
                 <v-list-item :title="$t('about-app')" @click="$router.push('/settings/info')">
                     <template v-slot:prepend>
-                        <v-icon color="primary">mdi-information-outline</v-icon>
+                        <v-icon icon="pet:alert-circle" class="settings-icon" color="primary"> </v-icon>
                     </template>
 
                     <template v-slot:append>
-                        <v-btn color="grey-lighten-1" icon="mdi-chevron-right" variant="text"></v-btn>
+                        <v-btn color="grey-lighten-1" icon="pet:chevron-right" variant="text"></v-btn>
                     </template>
                 </v-list-item>
                 <v-list-item title="Выйти">
                     <template v-slot:prepend>
-                        <v-icon color="primary">mdi-logout</v-icon>
+                        <v-icon icon="pet:log-out" class="settings-icon" color="primary"></v-icon>
                     </template>
 
                     <template v-slot:append>
-                        <v-btn color="grey-lighten-1" icon="mdi-chevron-right" variant="text"></v-btn>
+                        <v-btn color="grey-lighten-1" icon="pet:chevron-right" variant="text"></v-btn>
                     </template>
                 </v-list-item>
             </v-list>
@@ -87,31 +88,36 @@ export default {
 </script>
 
 <style scoped>
+.v-container {
+    padding: 16px 8px;
+}
+
+.v-list-item {
+    padding: 0px !important;
+}
+
 .v-list-item.avatar-item {
-    padding: 12px;
+    padding: 12px !important;
     border-radius: 12px;
     border: 1px;
 }
 
 .settings-icon {
-    width: 25px;
+    margin-right: 14px !important;
 }
 
-.v-bottom-navigation {
+.top-nav {
     top: 0px;
+    width: 100%;
+    height: 56px;
     color: gray;
 }
 
 .v-bottom-navigation i {
-    font-size: 40px;
-}
-
-.settings-page {
-    margin-top: 56px;
+    font-size: 25px;
 }
 
 .over-driver {
     width: 150%;
     margin-left: -20%;
-}
-</style>
+}</style>
