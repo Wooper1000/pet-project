952447beef81356eed73e152a3aa0e0b7c7f6f7f
Roman <romanProger@mail.ru>
2023-05-30 15:52:53 +0000

    Обновил набор иконок

--- src/assets/pet-icons/icons.js
+++ src/assets/pet-icons/icons.js
@@ -1,7 +1,7 @@
 import { h } from "vue";
 
 const petIcons = {
-  component:  (props) => h('i', {class: `pet-icon-${props.icon}`})
+  component:  (props) => h('i', {class: `pet-${props.icon}`})
 };
 
 export default petIcons;
