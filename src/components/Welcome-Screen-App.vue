1d92cf9d52796c493ee38d29adf169b17c8737ab
Wooper1000 <woper1000@gmail.com>
2023-05-30 23:48:01 +0300

    logo

--- src/components/Welcome-Screen-App.vue
+++ src/components/Welcome-Screen-App.vue
@@ -1,6 +1,8 @@
 <template>
     <v-main class="d-flex align-center mb-6">
-      <v-container  class="text-center" >
+      <v-container  class="text-center">
+        <video class='logo' v-if="!isSafari" :src="require('@/assets/imgs/logo.webm')" autoplay muted/>
+        <v-img class='safari-logo' :src="require('@/assets/imgs/logo.png')" v-if="isSafari"/>
         <h1 class="text-primary">{{ $t('greeting') }}</h1>
         <h4 class="mt-7">
          {{ $t('app-description') }}
@@ -16,10 +18,26 @@
 </template>
 <script>
 export default {
-  name: "WelcomeScreenApp"
+  name: "WelcomeScreenApp",
+  created() {
+    this.isSafari = navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome');
+  },
+  data(){
+    return {
+      isSafari:false
+    }
+  }
 };
 </script>
 
 <style>
+.logo{
+  width: 90%;
+}
 
+.safari-logo {
+  width: 60%;
+  margin: auto;
+  margin-bottom: 40px;
+}
 </style>
