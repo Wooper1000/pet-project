4b17e4f51803fc174571a1198fb34a487081dac7
Wooper1000 <woper1000@gmail.com>
2023-05-30 10:54:27 +0300

    router and api reworks. Added redirects when token expires

--- src/api.js
+++ src/api.js
@@ -1,105 +1,111 @@
 import axios from 'axios';
+import router from '@/router';
 
-let headers = {
+const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json'
 };
-let config = {
-    baseURL: 'http://91.122.48.173:9090/api/v1',
-    headers,
-};
+
+const baseURL = 'http://91.122.48.173:9090/api/v1';
+
 let token = localStorage.getItem('token');
 
-if(token){
-    config.headers['Authorization'] = `Bearer ${token}`;
-}
+const apiClient = axios.create({
+    baseURL,
+    headers,
+    ...(token && { headers: { ...headers, Authorization: `Bearer ${token}` } })
+});
 
-let apiClient = axios.create(config);
+apiClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 403) {
+            delete apiClient.defaults.headers.common['Authorization'];
+            localStorage.removeItem('token');
+            router.push('/login');
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default {
     registerUser(user) {
-        this.resetToken();
         return apiClient.post('/auth/register', user);
     },
-    resetToken(){
-        delete config.headers['Authorization'];
-        apiClient = axios.create(config);
-    },
+
     async loginUser(user) {
-        let response = null;
+        const response = await apiClient.post('/auth', user);
 
-        this.resetToken();
-        response = await apiClient.post('/auth', user)
-
-        try{
-            let token = response.data.token;
-
-            localStorage.setItem('token',token);
-            config.headers['Authorization'] = `Bearer ${token}`;
-            apiClient = axios.create(config);
-        }catch(e){
-            console.log('test');
+        try {
+            const token = response.data.token;
+            localStorage.setItem('token', token);
+            apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        } catch (error) {
+            return {}
         }
-
-        return new Promise((res)=> res(response));
-    },
-    async getUser(){
-        let user = {};
-        let response = await apiClient.get('/users');
-
-        if(response.status !== 200){
-            this.$store.commit('showSnackbar', { text: response.data.message, color: 'error' });
-            return user;
-        }else{
-            return response.data;
-        }
-    },
-    async getUserTasks(){
-        let tasks = [];
-        let response = await apiClient.get('/tasks/user');
-
-        if(response.status !== 200){
-            return tasks;
-        }else{
-            return response.data;
-        }
-    },
-    async getFullTask(id){
-        let task = {};
-        let response = await apiClient.get('/tasks/' + id);
-
-        if(response.status !== 200){
-            return task;
-        }else{
-            return response.data;
-        }
-    },
-    async addTask(task){
-        let result = {};
-        let response = await apiClient.post('/tasks', task);
-
-        if(response.status !== 200){
-            return result;
-        }else{
-            return response.data;
-        }
-    },
-    async getStructureOnAddress(addr, start, end){
-        let url = "http://188.143.130.20:3000/";
-        let params = `?address=${addr}&start=${start}&end=${end}`;
-        let resp = await axios.get(url + params);
-
-        return resp.data;
-    },
-    async replaceSubTasks(params){
-        let response = apiClient.patch(`/tasks/${params.taskId}/subtasks/replace`, params, {
-            params,
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
 
         return response;
+    },
+
+    async getUser() {
+        try {
+            const response = await apiClient.get('/users');
+            return response.data;
+        } catch (error) {
+            return {};
+        }
+    },
+
+    async getUserTasks() {
+        try {
+            const response = await apiClient.get('/tasks/user');
+            return response.data;
+        } catch (error) {
+            return [];
+        }
+    },
+
+    async getFullTask(id) {
+        try {
+            const response = await apiClient.get(`/tasks/${id}`);
+            return response.data;
+        } catch (error) {
+            return {};
+        }
+    },
+
+    async addTask(task) {
+        try {
+            const response = await apiClient.post('/tasks', task);
+            return response.data;
+        } catch (error) {
+            return {};
+        }
+    },
+
+    async getStructureOnAddress(addr, start, end) {
+        const url = 'http://188.143.130.20:3000/';
+        const params = `?address=${addr}&start=${start}&end=${end}`;
+
+        try {
+            const response = await axios.get(url + params);
+            return response.data;
+        } catch (error) {
+            return {};
+        }
+    },
+
+    async replaceSubTasks(params) {
+        try {
+            const response = await apiClient.patch(`/tasks/${params.taskId}/subtasks/replace`, params, {
+                params,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            return {};
+        }
     }
-    // другие методы для работы с API
 };
